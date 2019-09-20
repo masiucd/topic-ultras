16d41bd3edf44e070fe8756a55b58adeda4da4eb
Masiu <ciszekmarcell@gmail.com>
2019-09-20 19:14:58 +0200

    load user profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,8 +1,15 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-shadow */
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = props => {
-  let a;
+const Dashboard = ({ getCurrentProfile, profile, auth }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
@@ -10,6 +17,15 @@ const Dashboard = props => {
   );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({ profile: state.profile, auth: state.auth });
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,9 +1,26 @@
-const initialState = {};
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case 'value':
-      return {};
-
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
     default:
       return state;
   }
