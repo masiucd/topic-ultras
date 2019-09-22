a57d2121d1ecff5c9ea4c4232c912b25b0725051
Masiu <ciszekmarcell@gmail.com>
2019-09-22 17:49:24 +0200

    get profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,12 +6,25 @@ import {
   UPDATE_PROFILE,
   CLEAR_PROFILE,
   ACCOUNT_DELETED,
+  GET_PROFILES,
 } from './types';
 
 export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get('/api/profile/me');
     dispatch({ type: GET_PROFILE, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const getProfiles = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profile');
+    dispatch({ type: GET_PROFILES, payload: res.data });
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
