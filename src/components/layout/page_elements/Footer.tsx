0d97d581351df47cc3991462bd95f7ecd0bc59c4
Masiu <ciszekmarcell@gmail.com>
2020-06-13 19:57:26 +0200

    footer logic

--- src/components/layout/page_elements/Footer.tsx
+++ src/components/layout/page_elements/Footer.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { useStaticQuery, graphql } from 'gatsby'
+import { IFixedObject } from 'gatsby-background-image'
 import FooterList from './FooterList'
 import { handleFlex } from '../../styles/utils/helpers'
-
-interface Props {}
+import SocialList from './SocialList'
 
 const FooterStyles = styled.footer`
   background: ${props => props.theme.colors.black};
   color: ${props => props.theme.colors.white};
   padding: 2rem 1rem;
   ${handleFlex('row', 'space-between', 'center')};
+  @media (max-width: 960px) {
+    ${handleFlex('column', 'center', 'center')};
+  }
 `
 
 const FooterTitle = styled.div`
   padding: 1rem;
+  @media (max-width: 960px) {
+    ${handleFlex('row', 'center', 'center')};
+    span {
+      padding: 0 1.5rem;
+    }
+  }
 `
 
 const FOOTER_QUERY = graphql`
@@ -28,12 +37,34 @@ const FOOTER_QUERY = graphql`
         }
       }
     }
+    social: allFile(filter: { extension: { eq: "png" } }) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            fixed(width: 35, height: 35) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
   }
 `
 
 interface Link<T> {
   name: T
   path: T
+}
+interface SocialNode {
+  node: {
+    id: string
+    name: string
+    childImageSharp: {
+      fixed: IFixedObject
+    }
+  }
 }
 
 interface FooterQuery {
@@ -43,18 +74,26 @@ interface FooterQuery {
       links: Array<Link<string>>
     }
   }
+  social: {
+    edges: Array<SocialNode>
+  }
 }
 
-const Footer: React.FC<Props> = () => {
+const Footer: React.FC = () => {
   const {
     siteData: { siteMetadata },
+    social: { edges: socialXs },
   } = useStaticQuery<FooterQuery>(FOOTER_QUERY)
+  const date = new Date()
+
   return (
     <FooterStyles>
       <FooterTitle>
         <h1>{siteMetadata.title}</h1>
+        <span>&#169;{date.getFullYear()}</span>
       </FooterTitle>
       <FooterList onLinks={siteMetadata.links} />
+      <SocialList onSocialXs={socialXs} />
     </FooterStyles>
   )
 }
