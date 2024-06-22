a92b29e6a6179eae72c0ff80131fcedd28b53b0b
masiucd <ciszekmarcell@gmail.com>
2024-06-22 13:40:05 +0200

    feat: Refactor searchFood function to use selectFoodSchema for parsing query result
    The searchFood function in search-food.ts has been refactored to use the selectFoodSchema from the schema file for parsing the query result. This ensures that the result is properly validated and provides better error handling. Additionally, the function now logs the result data to the console before returning it.
    
    These changes improve the reliability and maintainability of the searchFood function.

--- src/actions/search-food.ts
+++ src/actions/search-food.ts
@@ -4,17 +4,30 @@ import "server-only";
 import {sql} from "drizzle-orm";
 
 import {db} from "@/db/db";
-import {foods} from "@/db/models/schema";
+import {foods, type SelectFood, selectFoodSchema} from "@/db/models/schema";
 
-export async function searchFood(prevState: any, formData: FormData) {
+export async function searchFood(
+  prevState: null | SelectFood,
+  formData: FormData,
+) {
   let food = formData.get("food");
   if (typeof food !== "string") {
     throw new Error("Expected food to be a string.");
   }
-  let result = await db
-    .select()
-    .from(foods)
-    .where(sql`lower(${foods.name}) = lower(${food})`);
+  let result = selectFoodSchema.safeParse(
+    await db
+      .select()
+      .from(foods)
+      .where(sql`lower(${foods.name}) = lower(${food})`)
+      .get(),
+  );
 
-  return result;
+  if (result.success) {
+    console.log("result", result.data);
+    return result.data;
+  }
+
+  return null;
 }
+
+export type SearchFood = typeof searchFood;
---
+++ src/app/search-food.tsx
@@ -0,0 +1,69 @@
+"use client";
+import type {PropsWithChildren} from "react";
+import {useFormState, useFormStatus} from "react-dom";
+
+import {searchFood} from "@/actions/search-food";
+import {cn} from "@/lib/cn";
+import {Button} from "@/shared/components/ui/button";
+
+// TODO save state in url params
+export function SearchFood() {
+  let [foodResult, action] = useFormState(searchFood, null);
+
+  return (
+    <div>
+      <div>SearchFood</div>
+      <FoodForm action={action} />
+      {foodResult !== null && (
+        <div>
+          <h3>{foodResult.name}</h3>
+          <p>{foodResult.servingSize}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function FoodForm({
+  action,
+}: {
+  // eslint-disable-next-line no-unused-vars
+  action: (formData: FormData) => void;
+}) {
+  return (
+    <form
+      action={action}
+      className="flex flex-col border border-blue-400 md:w-full md:max-w-xl"
+    >
+      <input
+        type="text"
+        datatype="text"
+        placeholder="Search for food"
+        name="food"
+        required
+      />
+      <SubmitButton>
+        <span>search</span>
+      </SubmitButton>
+    </form>
+  );
+}
+
+type SubmitButtonProps = {
+  className?: string;
+};
+function SubmitButton({
+  className,
+  children,
+}: PropsWithChildren<SubmitButtonProps>) {
+  let {pending} = useFormStatus();
+  return (
+    <Button
+      isDisabled={pending}
+      className={cn("font-semibold capitalize", className)}
+      type="submit"
+    >
+      {children}
+    </Button>
+  );
+}
