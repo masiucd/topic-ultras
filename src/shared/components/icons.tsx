1c11f205ab59f74b5f588bd72feb34ea9381e4fc
masiucd <ciszekmarcell@gmail.com>
2024-06-23 18:02:53 +0200

    feat: Add icons for macronutrients in shared/components/icons.tsx
    This commit adds icons for macronutrients (Carbs, Protein, Fat, Calorie) in the shared/components/icons.tsx file. The icons are imported from the lucide-react library and stored in the Icons object. This addition enhances the visual representation of macronutrients in the application.

---
+++ src/shared/components/icons.tsx
@@ -0,0 +1,13 @@
+import {
+  Zap as Carbs,
+  Beef as Protein,
+  Droplet as Fat,
+  Flame as Calorie,
+} from "lucide-react";
+
+export let Icons = Object.freeze({
+  Carbs,
+  Protein,
+  Fat,
+  Calorie,
+});
