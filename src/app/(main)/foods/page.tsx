7f08d5744cdc258b17532cdb1918f4e9685d0c12
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-06-28 09:27:24 +0200

    Refactor page component to use Radix UI components
    The page component in page.tsx has been refactored to use Radix UI components such as Flex and Table. This change improves the component's functionality and provides a more consistent and modern UI design.
    
    These changes enhance the user experience and maintainability of the codebase.

--- src/app/(main)/foods/page.tsx
+++ src/app/(main)/foods/page.tsx
@@ -1,16 +1,47 @@
+import {Flex, Table} from "@radix-ui/themes";
+
+import {PageWrapper} from "@/shared/components/page-wrapper";
 import {H1} from "@/shared/components/ui/typography";
 
+// Display table of food items with pagination and save the search state in the URL
 export default async function FoodItemsPage({
   searchParams,
 }: {
   searchParams: {[key: string]: string | string[] | undefined};
 }) {
+  console.log("🚀 ~ searchParams:", searchParams);
   // let result = await db.select().from(foods).all();
   // console.log("result", result);
 
   return (
-    <main className="mx-auto flex min-h-screen max-w-6xl flex-col items-center justify-center border-4 py-2">
+    <PageWrapper>
       <H1>Food items</H1>
-    </main>
+
+      <Flex direction="column" gap="5" maxWidth="600px">
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Food</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Calories</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Carbohydrates</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Fat</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Protein</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.RowHeaderCell>Apple</Table.RowHeaderCell>
+              <Table.Cell>Red apple</Table.Cell>
+              <Table.Cell>95</Table.Cell>
+              <Table.Cell>25g</Table.Cell>
+              <Table.Cell>0.3g</Table.Cell>
+              <Table.Cell>0.5g</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table.Root>
+      </Flex>
+    </PageWrapper>
   );
 }
