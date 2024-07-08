7643944c79f40cd73318fab20c338f1de2c8d6c1
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-07-08 15:55:01 +0200

    rebase

--- src/app/(main)/food-item.tsx
+++ src/app/(main)/food-item.tsx
@@ -20,7 +20,6 @@ export function FoodItem({
   let {foodName, description, calories, carbs, totalFat, protein, foodType} =
     food;
   return (
-<<<<<<< HEAD
     <li
       className={cn(
         "relative flex flex-col gap-2 rounded-md border border-gray-900 px-2 py-3 shadow-md",
@@ -44,65 +43,6 @@ export function FoodItem({
         protein={protein}
       />
     </li>
-=======
-    <Card asChild size="2" variant="surface">
-      <li className={cn("relative", className)}>
-        <FoodTypeBadge className="absolute right-2 top-2" foodType={foodType} />
-        <Flex direction="column" gap="1">
-          <Link href={`/foods/${slugify(food.foodName)}`}>
-            <Strong>{foodName}</Strong>
-          </Link>
-          <Tooltip content="asd">
-            <P size="2" truncate>
-              {description}
-            </P>
-          </Tooltip>
-        </Flex>
-        <Separator my="2" size="4" />
-
-        <DataList.Root>
-          <DataList.Item>
-            <DataList.Label minWidth="88px">
-              <Tooltip content="Calories">
-                <Icons.Calorie size={ICON_SIZE} />
-              </Tooltip>
-            </DataList.Label>
-            <DataList.Value>{calories}</DataList.Value>
-          </DataList.Item>
-          <DataList.Item>
-            <DataList.Label minWidth="88px">
-              <Tooltip content="Carbohydrates">
-                <Icons.Carbs size={ICON_SIZE} />
-              </Tooltip>
-            </DataList.Label>
-            <DataList.Value>
-              <Span>{carbs}</Span>
-            </DataList.Value>
-          </DataList.Item>
-          <DataList.Item>
-            <DataList.Label minWidth="88px">
-              <Tooltip content="Total Fat">
-                <Icons.Fat size={ICON_SIZE} />
-              </Tooltip>
-            </DataList.Label>
-            <DataList.Value>
-              <Span>{totalFat}</Span>
-            </DataList.Value>
-          </DataList.Item>
-          <DataList.Item>
-            <DataList.Label minWidth="88px">
-              <Tooltip content="Protein">
-                <Icons.Protein size={ICON_SIZE} />
-              </Tooltip>
-            </DataList.Label>
-            <DataList.Value>
-              <Span>{protein}</Span>
-            </DataList.Value>
-          </DataList.Item>
-        </DataList.Root>
-      </li>
-    </Card>
->>>>>>> da14c94 (Refactor food-item component to use consistent URL format)
   );
 }
 
