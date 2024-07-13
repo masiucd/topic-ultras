4019d967f2c48c2d13507653ffe4f64b42181b87
masiucd <ciszekmarcell@gmail.com>
2024-07-13 20:03:55 +0200

    Refactor import paths in food and food_type DAOs

--- src/site-data.ts
+++ src/site-data.ts
@@ -1,3 +1,6 @@
+import type {ThemeProps} from "@radix-ui/themes";
+import type {Route} from "next";
+
 export let siteData = Object.freeze({
   title: "Nutri Check",
   description: "Nutrition facts for your favorite foods.",
@@ -6,5 +9,13 @@ export let siteData = Object.freeze({
     {href: "/foods", text: "Food List"},
     {href: "/about", text: "About"},
     {href: "/contact", text: "Contact"},
-  ],
+  ] as {href: Route<string>; text: string}[],
+});
+
+export let themeData: ThemeProps = Object.freeze({
+  panelBackground: "translucent",
+  accentColor: "iris",
+  grayColor: "slate",
+  radius: "medium",
+  scaling: "95%",
 });
