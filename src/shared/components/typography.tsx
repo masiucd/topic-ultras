d2a7d5ced9a631450de3e2c89e6d8db54764f9ef
masiucd <ciszekmarcell@gmail.com>
2024-07-15 09:09:58 +0200

    Refactor import paths in food and food_type DAOs

--- src/shared/components/typography.tsx
+++ src/shared/components/typography.tsx
@@ -1,56 +1,129 @@
-import {
-  Heading,
-  type HeadingProps,
-  Strong as RadixStrong,
-  type StrongProps,
-  Text,
-  type TextProps,
-} from "@radix-ui/themes";
+import type {PropsWithChildren} from "react";
 
 import {cn} from "@/shared/lib/cn";
 
-export function H1(props: HeadingProps) {
-  return <Heading weight="bold" size="9" as="h1" {...props} />;
-}
-
-export function H2(props: HeadingProps) {
-  return <Heading weight="bold" size="8" as="h2" {...props} />;
-}
-
-export function H3(props: HeadingProps) {
-  return <Heading weight="bold" size="7" as="h3" {...props} />;
-}
-
-export function H4(props: HeadingProps) {
-  return <Heading weight="bold" size="6" as="h4" {...props} />;
-}
-
-export function H5(props: HeadingProps) {
-  return <Heading weight="bold" size="5" as="h5" {...props} />;
-}
-
-export function H6(props: HeadingProps) {
-  return <Heading weight="bold" size="4" as="h6" {...props} />;
-}
-
-export function P(props: TextProps) {
-  return <Text size="4" as="p" {...props} />;
-}
-
-export function Span(props: TextProps) {
-  return <Text as="span" {...props} />;
-}
-
-export function Label(props: TextProps) {
+export function H1(
+  props: PropsWithChildren<{
+    className?: string;
+  }>,
+) {
   return (
-    <Text
-      className={cn("block text-sm font-medium", props.className)}
-      as="label"
-      {...props}
-    />
+    <h1
+      className={cn(
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        props.className,
+      )}
+    >
+      {props.children}
+    </h1>
   );
 }
 
-export function Strong(props: StrongProps) {
-  return <RadixStrong {...props} />;
+export function H2(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <h2
+      className={cn(
+        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        props.className,
+      )}
+    >
+      {props.children}
+    </h2>
+  );
+}
+
+export function H3(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl font-semibold tracking-tight",
+        props.className,
+      )}
+    >
+      {props.children}
+    </h3>
+  );
+}
+
+export function H4(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <h4
+      className={cn(
+        "scroll-m-20 text-xl font-semibold tracking-tight",
+        props.className,
+      )}
+    >
+      {props.children}
+    </h4>
+  );
+}
+
+export function P(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <p className={cn("leading-7 [&:not(:first-child)]:mt-6", props.className)}>
+      {props.children}
+    </p>
+  );
+}
+
+export function Blockquote(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <blockquote className={cn("mt-6 border-l-2 pl-6 italic", props.className)}>
+      {props.children}
+    </blockquote>
+  );
+}
+
+export function List(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-2", props.className)}>
+      {props.children}
+    </ul>
+  );
+}
+
+export function InlineCode(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <code
+      className={cn(
+        "bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        props.className,
+      )}
+    >
+      {props.children}
+    </code>
+  );
+}
+
+export function Lead(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <p className={cn("text-muted-foreground text-xl", props.className)}>
+      A modal dialog that interrupts the user with important content and expects
+      a response.
+    </p>
+  );
+}
+
+export function Large(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <div className={cn("text-lg font-semibold", props.className)}>
+      {props.children}
+    </div>
+  );
+}
+
+export function Small(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <small className={cn("text-sm font-medium leading-none", props.className)}>
+      {props.children}
+    </small>
+  );
+}
+
+export function Muted(props: PropsWithChildren<{className?: string}>) {
+  return (
+    <p className={cn("text-muted-foreground text-sm", props.className)}>
+      {props.children}
+    </p>
+  );
 }
