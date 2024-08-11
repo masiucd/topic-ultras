eb0b2c4821e3ac0cb0fb5542c9e79823dbeab551
masiucd <ciszekmarcell@gmail.com>
2024-08-11 16:19:11 +0200

    feat: Add lazy loading for images in FoodNamePage
    - Lazy load images in the FoodNamePage component to improve page load performance.
    - Imported the necessary components and dependencies for lazy loading.
    - Updated the page.tsx file to use the lazy loading components for images.

--- src/app/(main)/foods/[foodname]/page.tsx
+++ src/app/(main)/foods/[foodname]/page.tsx
@@ -1,9 +1,22 @@
 import {eq} from "drizzle-orm";
 import {redirect} from "next/navigation";
+import type {ReactNode} from "react";
 
-import {H1, P} from "@/components/typography";
+import {Span} from "@/components/typography";
+import {Button} from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {FoodTypeBadge} from "@/components/ui/food-type-badge";
+import {Icons} from "@/components/ui/icons";
+import {Separator} from "@/components/ui/sperator";
 import {db} from "@/db";
-import {foods} from "@/db/schema";
+import {foodNutrients, foods, foodTypes} from "@/db/schema";
 
 async function getFoodItemByName(foodName: string) {
   try {
@@ -11,8 +24,20 @@ async function getFoodItemByName(foodName: string) {
       .select({
         name: foods.name,
         description: foods.description,
+        type: {
+          name: foodTypes.name,
+          id: foodTypes.id,
+        },
+        nutrients: {
+          calories: foodNutrients.calories,
+          fat: foodNutrients.fat,
+          protein: foodNutrients.protein,
+          carbs: foodNutrients.carbs,
+        },
       })
       .from(foods)
+      .innerJoin(foodTypes, eq(foods.typeId, foodTypes.id))
+      .innerJoin(foodNutrients, eq(foods.id, foodNutrients.foodId))
       .where(eq(foods.slug, foodName));
 
     return item[0];
@@ -34,8 +59,83 @@ export default async function FoodNamePage({
   }
   return (
     <div>
-      <H1 className="capitalize">{food.name}</H1>
-      <P>{food.description}</P>
+      <FoodCard foodItem={food} />
     </div>
+  );
+}
+
+type FoodItem = NonNullable<Awaited<ReturnType<typeof getFoodItemByName>>>;
+function FoodCard({foodItem}: {foodItem: FoodItem}) {
+  return (
+    <Card className="max-w-2xl ">
+      <CardHeader className="relative">
+        <CardTitle className="flex  items-center justify-between">
+          <Span className="text-4xl capitalize">Nutrition Facts</Span>
+          <Button size="sm">
+            <Icons.Star />
+          </Button>
+        </CardTitle>
+        <CardDescription className="text-lg">
+          {/* {foodItem.description} */}
+          Serving size: 100g (3.5 oz)
+        </CardDescription>
+      </CardHeader>
+      <div className="mb-2 px-6">
+        <Separator />
+      </div>
+      <CardContent>
+        {/* TODO add vizual diagram */}
+        <ul className="flex max-w-60 flex-col gap-4 pe-1.5">
+          <ListItem
+            iconAndLabel={{icon: <Icons.Food />, label: "Food"}}
+            value={foodItem.name}
+          />
+          <ListItem
+            iconAndLabel={{icon: <Icons.Label />, label: "Type"}}
+            value={<FoodTypeBadge name={foodItem.type.name} />}
+          />
+          <ListItem
+            iconAndLabel={{icon: <Icons.Calories />, label: "Calories"}}
+            value={foodItem.nutrients.calories}
+          />
+          <ListItem
+            iconAndLabel={{icon: <Icons.Fat />, label: "Total Fat"}}
+            value={foodItem.nutrients.fat}
+          />
+          <ListItem
+            iconAndLabel={{icon: <Icons.Protein />, label: "Protein"}}
+            value={foodItem.nutrients.protein}
+          />
+          <ListItem
+            iconAndLabel={{icon: <Icons.Carbs />, label: "Carbs"}}
+            value={foodItem.nutrients.carbs}
+          />
+        </ul>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        {/* <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button> */}
+      </CardFooter>
+    </Card>
+  );
+}
+
+function ListItem({
+  iconAndLabel,
+  value,
+}: {
+  iconAndLabel: {icon: ReactNode; label: string};
+  value: string | ReactNode;
+}) {
+  return (
+    <li className="flex flex-col ">
+      <div className="mb-5 flex items-center justify-between">
+        <Span className="flex items-center gap-1 font-semibold">
+          {iconAndLabel.icon} {iconAndLabel.label}:
+        </Span>
+        <Span className="capitalize">{value}</Span>
+      </div>
+      <Separator />
+    </li>
   );
 }
