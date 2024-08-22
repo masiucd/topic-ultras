0f1400351f397324de1c1c33877a319931a23ee5
masiucd <ciszekmarcell@gmail.com>
2024-08-22 18:03:29 +0200

    refactor: Improve HeadTitle component in foods page
    Refactor the HeadTitle component in src/app/(main)/foods/_components/head-title.tsx to improve its functionality. The changes include importing the "cn" utility function from "@/lib/utils" and adding a conditional class name to the Link component based on the "orderBy" parameter. This allows for better styling and visual indication of the currently selected sorting option in the foods page.

--- src/app/(main)/foods/_components/head-title.tsx
+++ src/app/(main)/foods/_components/head-title.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import {usePathname, useSearchParams} from "next/navigation";
 import type {PropsWithChildren} from "react";
 
+import {cn} from "@/lib/utils";
+
 export function HeadTitle({
   title,
   children,
@@ -13,12 +15,21 @@ export function HeadTitle({
 }>) {
   let searchParams = useSearchParams();
   let pathName = usePathname();
+
   let params = new URLSearchParams(searchParams);
-  params.set("orderby", title);
+  let orderBy = params.get("orderby");
+
   if (title === "name") {
     params.delete("orderby");
+  } else {
+    params.set("orderby", title);
   }
+
   let href = `${pathName}?${params.toString()}` as Route<string>;
 
-  return <Link href={href}>{children}</Link>;
+  return (
+    <Link className={cn(orderBy === title && "text-blueA11")} href={href}>
+      {children}
+    </Link>
+  );
 }
