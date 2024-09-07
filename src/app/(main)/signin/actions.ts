a87d3ee659af2e825823a30975ffc396c5277a54
masiucd <ciszekmarcell@gmail.com>
2024-09-07 16:35:32 +0200

    refactor: Use Redis for session storage and improve login functionality

--- src/app/(main)/signin/actions.ts
+++ src/app/(main)/signin/actions.ts
@@ -1,16 +1,17 @@
 "use server";
 import "server-only";
 
+// import {parseISO} from "date-fns";
 import {eq} from "drizzle-orm";
 import {redirect} from "next/navigation";
 
 import {db} from "@/db";
-import {users} from "@/db/schema";
-import {getExpiresInHours, setCookie} from "@/lib/cookies";
+import {sessions, users} from "@/db/schema";
+import {createExpirationDate, getExpiresInHours, setCookie} from "@/lib/cookies";
 import {encrypt} from "@/lib/crypto";
 import {verifyPassword} from "@/lib/password";
 
-export async function login(prevState: {message: string} | null, data: FormData) {
+export async function login(prevState: {ok: boolean} | null, data: FormData) {
   let email = data.get("email");
   let password = data.get("password");
   if (typeof email !== "string" || typeof password !== "string") {
@@ -18,24 +19,28 @@ export async function login(prevState: {message: string} | null, data: FormData)
   }
   let user = await getUserByEmail(email);
   if (user === null) {
-    return {message: "Wrong email or password"};
+    return {ok: false};
   }
   let isValidPassword = await verifyPassword(password, user.password);
   if (!isValidPassword) {
-    return {message: "Wrong email or password"};
+    return {ok: false};
   }
+  // TODO use Reddis for session storage
+  let token = await encrypt({
+    id: user.id,
+    email: user.email,
+    iat: Date.now(),
+    exp: getExpiresInHours(2),
+  });
 
-  setCookie(
-    "session",
-    await encrypt({
-      id: user.id,
-      email: user.email,
-      iat: Date.now(),
-      exp: getExpiresInHours(2),
-    }),
-  );
+  // setCookie("session", token);
+  let resultToken = await insertIntoSessions(user.id, token);
 
-  redirect("/dashboard");
+  if (resultToken !== null) {
+    setCookie("session", resultToken.token);
+    redirect("/dashboard");
+  }
+  return {ok: true};
 }
 
 async function getUserByEmail(email: string) {
@@ -51,6 +56,25 @@ async function getUserByEmail(email: string) {
     } else {
       return null;
     }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return null;
+  }
+}
+
+async function insertIntoSessions(userId: number, token: string) {
+  try {
+    let res = await db
+      .insert(sessions)
+      .values({
+        userId,
+        token,
+        expiresAt: createExpirationDate(),
+        // expires: createExpirationDate(),
+      })
+      .returning({token: sessions.token});
+    return res[0];
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
