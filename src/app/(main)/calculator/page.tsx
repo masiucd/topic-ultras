a0b4b451dc5506bb062251878f617c060e04d906
masiucd <ciszekmarcell@gmail.com>
2024-09-08 17:12:15 +0200

    feat: Add CalculatorPage component
    Add a new component for the Calculator page where users can calculate their daily intake of nutrients and calories based on their goals and activity level.

--- src/app/(main)/calculator/page.tsx
+++ src/app/(main)/calculator/page.tsx
@@ -1,5 +1,9 @@
 import PageWrapper from "@/components/page-wrapper";
 
+/**
+ * Calculator page
+ * Where the user can calculate their daily intake of nutrients and calories based on their goals and activity level
+ */
 export default function CalculatorPage() {
-  return <PageWrapper>CalculatorPage</PageWrapper>;
+	return <PageWrapper>CalculatorPage</PageWrapper>;
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,83 +11,88 @@ import {cn} from "@/lib/utils";
 import {logout} from "./actions";
 
 export default function MainLayout({
-  children,
+	children,
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode;
 }>) {
-  return (
-    <>
-      <Header />
-      <main className="flex min-h-[calc(100dvh-10rem)] flex-col">{children}</main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main className="flex min-h-[calc(100dvh-10rem)] flex-col">{children}</main>
+			<Footer />
+		</>
+	);
 }
 
 function Header() {
-  return (
-    <header className="h-20">
-      <Wrapper justify="between">
-        <Link href="/" className="decoration-gray-700/45 hover:underline">
-          <Strong>{appData.title}</Strong>
-        </Link>
-        <Nav />
-      </Wrapper>
-    </header>
-  );
+	return (
+		<header className="h-20">
+			<Wrapper justify="between">
+				<Link href="/" className="decoration-gray-700/45 hover:underline">
+					<Strong>{appData.title}</Strong>
+				</Link>
+				<Nav />
+			</Wrapper>
+		</header>
+	);
 }
 
 async function Nav() {
-  let payload = await isAuthorized();
-  return (
-    <nav>
-      <Flex asChild gap="2" align="center">
-        <ul>
-          {appData.routes.map((route) => (
-            <li key={route.name}>
-              <ActiveLink href={route.href}>{route.name}</ActiveLink>
-            </li>
-          ))}
-          {payload !== null ? (
-            <>
-              <li>
-                <ActiveLink href="/dashboard">Dashboard</ActiveLink>
-              </li>
-              <li className="ml-1">
-                <form action={logout}>
-                  <RadixLink asChild size="2" weight="medium">
-                    <button type="submit">Logout</button>
-                  </RadixLink>
-                </form>
-              </li>
-            </>
-          ) : (
-            <li>
-              <ActiveLink href="/signin">Sign in</ActiveLink>
-            </li>
-          )}
-        </ul>
-      </Flex>
-    </nav>
-  );
+	let payload = await isAuthorized();
+	return (
+		<nav>
+			<Flex asChild gap="2" align="center">
+				<ul>
+					{appData.routes.map((route) => (
+						<li key={route.name}>
+							<ActiveLink href={route.href}>{route.name}</ActiveLink>
+						</li>
+					))}
+					{payload !== null ? (
+						<>
+							<li>
+								<ActiveLink href="/dashboard">Dashboard</ActiveLink>
+							</li>
+							<li>
+								<ActiveLink href="/meal-planer">Meal planer</ActiveLink>
+							</li>
+							<li className="ml-1">
+								<form action={logout}>
+									<RadixLink asChild size="2" weight="medium">
+										<button type="submit">Logout</button>
+									</RadixLink>
+								</form>
+							</li>
+						</>
+					) : (
+						<li>
+							<ActiveLink href="/signin">Sign in</ActiveLink>
+						</li>
+					)}
+				</ul>
+			</Flex>
+		</nav>
+	);
 }
 
 function Footer() {
-  return (
-    <footer className="h-20">
-      <Wrapper>
-        <small>
-          {appData.title} &copy; {new Date().getFullYear()}
-        </small>
-      </Wrapper>
-    </footer>
-  );
+	return (
+		<footer className="h-20">
+			<Wrapper>
+				<small>
+					{appData.title} &copy; {new Date().getFullYear()}
+				</small>
+			</Wrapper>
+		</footer>
+	);
 }
 
 function Wrapper(props: FlexProps) {
-  return (
-    <Flex asChild align="center" {...props}>
-      <div className={cn("mx-auto  h-full max-w-6xl", props.className)}>{props.children}</div>
-    </Flex>
-  );
+	return (
+		<Flex asChild align="center" {...props}>
+			<div className={cn("mx-auto  h-full max-w-6xl", props.className)}>
+				{props.children}
+			</div>
+		</Flex>
+	);
 }
