c5bacac13f83d68b67e99a08a8326595cd4dbb7d
masiucd <ciszekmarcell@gmail.com>
2024-09-14 16:12:01 +0200

    refactor: Update CalculatorPage component and add new UI components
    This commit refactors the CalculatorPage component by updating the imports and adding new UI components. The H1 component from "@/components/typography" is now imported along with the Span component. The Input and Select components from "@/components/ui" are also imported. Additionally, the Button and RadioGroup components from "@radix-ui/themes" are imported. The form in the CalculatorPage component is updated to include the new UI components and their respective labels and inputs. The calculate function is imported from "./actions". This refactor improves the structure and functionality of the CalculatorPage component.
    
    Ref: #123

--- src/app/(main)/calculator/page.tsx
+++ src/app/(main)/calculator/page.tsx
@@ -1,6 +1,9 @@
 import PageWrapper from "@/components/page-wrapper";
-import {H1} from "@/components/typography";
-import {Flex} from "@radix-ui/themes";
+import {H1, Span} from "@/components/typography";
+import {Input} from "@/components/ui/input";
+import {Select} from "@/components/ui/select";
+import {Button, Flex, RadioGroup} from "@radix-ui/themes";
+import {calculate} from "./actions";
 
 /**
  * Calculator page
@@ -10,51 +13,96 @@ export default function CalculatorPage() {
 	return (
 		<PageWrapper>
 			<H1>CalculatorPage</H1>
-			<Flex asChild gap="3">
-				<form action="">
-					<fieldset>
-						<legend>Personal information</legend>
-						<div>
-							<label htmlFor="age">Age</label>
-							<input type="number" name="age" id="age" />
-						</div>
-						<div>
-							<label htmlFor="gender">
-								Male
-								<input type="radio" name="gender" id="male" value="male" />
-							</label>
-							<label htmlFor="female">
-								Female
-								<input type="radio" name="gender" id="female" value="female" />
-							</label>
-						</div>
-						<div>
-							<label htmlFor="weight">Weight</label>
-							<input type="number" name="weight" id="weight" />
-						</div>
-						<div>
-							<label htmlFor="height">Height</label>
-							<input type="number" name="height" id="height" />
-						</div>
-						<div>
-							<label htmlFor="activity-level">Activity level</label>
-							<select name="activity-level" id="activity-level">
-								<option value="sedentary">Sedentary</option>
-								<option value="light">Light</option>
-								<option value="moderate">Moderate</option>
-								<option value="active">Active</option>
-								<option value="very-active">Very active</option>
-							</select>
-						</div>
-						<div>
-							<label htmlFor="goal">Goal</label>
-							<select name="goal" id="goal">
-								<option value="maintain">Maintain</option>
-								<option value="lose">Lose</option>
-								<option value="gain">Gain</option>
-							</select>
-						</div>
-					</fieldset>
+			<Flex maxWidth="500px" className="border border-red-500">
+				<form action={calculate} className="w-full">
+					<Flex direction="column" asChild gap="5" className="w-full bg-blue-300">
+						<fieldset>
+							<legend>Personal information</legend>
+							<Flex gap="3">
+								<div>
+									<label htmlFor="age">Age</label>
+									<Input
+										type="number"
+										name="age"
+										id="age"
+										min={5}
+										defaultValue={5}
+										required
+									/>
+								</div>
+								<RadioGroup.Root name="gender" highContrast required defaultValue="male">
+									<Flex align="center" className="h-full" gap="5">
+										<RadioGroup.Item id="male" value="male">
+											Male
+										</RadioGroup.Item>
+										<RadioGroup.Item id="female" value="female">
+											Female
+										</RadioGroup.Item>
+									</Flex>
+								</RadioGroup.Root>
+							</Flex>
+							<Flex gap="3">
+								<div>
+									<label htmlFor="weight">
+										Weight
+										<Span className="text-sm"> (kg)</Span>
+									</label>
+									<Input
+										type="number"
+										name="weight"
+										id="weight"
+										min={20}
+										max={400}
+										required
+									/>
+								</div>
+								<div>
+									<label htmlFor="height">
+										Height
+										<Span className="text-sm"> (cm)</Span>
+									</label>
+									<Input
+										type="number"
+										name="height"
+										id="height"
+										min={100}
+										max={250}
+										required
+									/>
+								</div>
+							</Flex>
+							<Flex gap="3">
+								<Select defaultValue="light" required name="activity-level">
+									<Select.Trigger />
+									<Select.Content>
+										<Select.Group>
+											<Select.Label>Activity Level</Select.Label>
+											<Select.Item value="sedentary">Sedentary</Select.Item>
+											<Select.Item value="light">Light</Select.Item>
+											<Select.Item value="moderate">Moderate</Select.Item>
+											<Select.Item value="active">Active</Select.Item>
+											<Select.Item value="very-active">Very active</Select.Item>
+										</Select.Group>
+									</Select.Content>
+								</Select>
+
+								<Select defaultValue="maintain" required name="goal">
+									<Select.Trigger />
+									<Select.Content>
+										<Select.Group>
+											<Select.Label>Goal</Select.Label>
+											<Select.Item value="maintain">Maintain</Select.Item>
+											<Select.Item value="lose">Lose</Select.Item>
+											<Select.Item value="gain">Gain</Select.Item>
+										</Select.Group>
+									</Select.Content>
+								</Select>
+							</Flex>
+							<Button highContrast type="submit" size="3">
+								<Span weight="bold">Calculate</Span>
+							</Button>
+						</fieldset>
+					</Flex>
 				</form>
 			</Flex>
 		</PageWrapper>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,18 +1,18 @@
 import type {SlotProps} from "@radix-ui/react-slot";
 import {TextField} from "@radix-ui/themes";
 import type {RootProps} from "@radix-ui/themes/dist/esm/components/text-field.js";
-import React, {forwardRef} from "react";
+import {forwardRef} from "react";
 
 export const Input = forwardRef<HTMLInputElement, RootProps>((props, ref) => {
-  return (
-    <TextField.Root type="text" {...props} ref={ref}>
-      {props.children}
-    </TextField.Root>
-  );
+	return (
+		<TextField.Root type="text" {...props} ref={ref}>
+			{props.children}
+		</TextField.Root>
+	);
 });
 
 Input.displayName = "Input";
 
 export function Slot(props: SlotProps) {
-  return <TextField.Slot>{props.children}</TextField.Slot>;
+	return <TextField.Slot>{props.children}</TextField.Slot>;
 }
