33b1ff69a0ca7fbcb608136a32f2322a3636e8f7
masiucd <ciszekmarcell@gmail.com>
2024-10-05 17:47:46 +0200

    setup db WIP

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from "drizzle-kit";
+
+import env from "@/env";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema/index.ts",
+  out: "./src/db/migrations",
+  dbCredentials: {
+    url: env.DB_URL,
+  },
+  breakpoints: true,
+  verbose: env.NODE_ENV === "development", // Log all queries in development
+  strict: true, // Enable strict mode
+});
