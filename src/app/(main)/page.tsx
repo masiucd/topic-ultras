6e843c6d8f951a3020209755d4ab34b0c3118e30
masiucd <ciszekmarcell@gmail.com>
2024-10-06 16:39:53 +0200

    Add database query for food items and categories

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,49 @@
 import PageWrapper from "@/components/page-wrapper";
+import {db} from "@/db";
+import {foodCategories, foodItems, foodNutrients} from "@/db/schema";
+import {eq} from "drizzle-orm";
+// import {setExpire} from "@/db/redis";
 
-export default function Home() {
+// async function redisTest(data: FormData) {
+//   "use server";
+//   let name = data.get("name");
+
+//   if (typeof name !== "string") {
+//     throw new Error("Name is not a string");
+//   }
+
+//   setExpire({key: "team", value: name});
+// }
+
+export default async function Home() {
+  let allFoodItems = await db
+    .select({
+      foodName: foodItems.name,
+      foodDescription: foodItems.description,
+      foodType: foodItems.foodType,
+      foodCategory: foodCategories.name,
+      nutrients: {
+        calories: foodNutrients.calories,
+        protein: foodNutrients.protein,
+        fat: foodNutrients.fat,
+        carbs: foodNutrients.carbs,
+      },
+    })
+    .from(foodItems)
+    .innerJoin(foodNutrients, eq(foodItems.id, foodNutrients.foodId))
+    .innerJoin(foodCategories, eq(foodItems.foodCategoryId, foodCategories.id));
+
   return (
     <PageWrapper className="border border-red-500">
-      <h1>Hello from hoome page</h1>
+      <h1>Nutri Check</h1>
+      <p>
+        Nutri Check application where you can track your nutrition and health.
+      </p>
+      <ul>
+        {allFoodItems.map((foodItem) => (
+          <li key={foodItem.foodName}>{foodItem.foodName}</li>
+        ))}
+      </ul>
     </PageWrapper>
   );
 }
