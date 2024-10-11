a38d06168b09c39f8ab77069ba2f573fba16c6f7
marcell ciszek <ciszekmarcell@gmail.com>
2024-10-11 14:39:36 +0200

    Add login and register pages

---
+++ src/app/(main)/log-in/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Register for Nutri Check</p>
+    </div>
+  );
+}
---
+++ src/app/(main)/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Register for Nutri Check</p>
+    </div>
+  );
+}
