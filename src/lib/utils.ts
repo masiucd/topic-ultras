c8b977e7fea888fcd928bbbc11095a37231879f9
masiucd <ciszekmarcell@gmail.com>
2024-10-13 16:06:41 +0200

    Refactor utils.ts to include debounce function for search input

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// debounce function that will be used in the search input
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  timeout = 300
+) {
+  let timer: number;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = window.setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
 }
