52feb25c283531ded5d277d06b0e652bbf7832f1
marcell ciszek <ciszekmarcell@gmail.com>
2024-10-15 20:01:31 +0200

    Refactor food item DAO and add utility functions

--- src/app/(main)/food-items/dao.ts
+++ src/app/(main)/food-items/dao.ts
@@ -4,59 +4,56 @@ import {foodCategories, foodItems, foodNutrients} from "@/db/schema";
 import {eq, ilike, sql} from "drizzle-orm";
 
 export async function getFoodItems({
-  name,
-  limit,
-  skip,
+	name,
+	limit,
+	skip,
 }: {
-  name?: string;
-  limit: number;
-  skip: number;
+	name?: string;
+	limit: number;
+	skip: number;
 }) {
-  return db.transaction(async (ctx) => {
-    try {
-      let totalFoodItems = await ctx
-        .select({
-          count: sql`count(*)`.mapWith(Number),
-        })
-        .from(foodItems);
+	return db.transaction(async (ctx) => {
+		try {
+			let totalFoodItems = await ctx
+				.select({
+					count: sql`count(*)`.mapWith(Number),
+				})
+				.from(foodItems);
 
-      let allFoodItems = await ctx
-        .select({
-          foodId: foodItems.id,
-          foodName: foodItems.name,
-          foodDescription: foodItems.description,
-          foodType: foodItems.foodType,
-          foodCategory: foodCategories.name,
-          nutrients: {
-            calories: foodNutrients.calories,
-            protein: foodNutrients.protein,
-            fat: foodNutrients.fat,
-            carbs: foodNutrients.carbs,
-          },
-        })
-        .from(foodItems)
-        .innerJoin(foodNutrients, eq(foodItems.id, foodNutrients.foodId))
-        .innerJoin(
-          foodCategories,
-          eq(foodItems.foodCategoryId, foodCategories.id)
-        )
-        .orderBy(foodItems.name)
-        .where(name ? ilike(foodItems.name, `%${name}%`) : undefined)
-        .limit(limit) // limit 4
-        .offset(skip); // skip 4
+			let allFoodItems = await ctx
+				.selectDistinct({
+					foodId: foodItems.id,
+					foodName: foodItems.name,
+					foodDescription: foodItems.description,
+					foodType: foodItems.foodType,
+					foodCategory: foodCategories.name,
+					nutrients: {
+						calories: foodNutrients.calories,
+						protein: foodNutrients.protein,
+						fat: foodNutrients.fat,
+						carbs: foodNutrients.carbs,
+					},
+				})
+				.from(foodItems)
+				.leftJoin(foodNutrients, eq(foodItems.id, foodNutrients.foodId))
+				.leftJoin(foodCategories, eq(foodItems.foodCategoryId, foodCategories.id))
+				.orderBy(foodItems.name)
+				.where(name ? ilike(foodItems.name, `%${name}%`) : undefined)
+				.limit(limit) // limit 4
+				.offset(skip); // skip 4
 
-      return {
-        totalFoodItems: totalFoodItems[0].count,
-        allFoodItems,
-      };
-    } catch (error) {
-      console.error("Error fetching food items:", error);
-      return {
-        totalFoodItems: 0,
-        allFoodItems: [],
-      };
-    }
-  });
+			return {
+				totalFoodItems: totalFoodItems[0].count,
+				allFoodItems,
+			};
+		} catch (error) {
+			console.error("Error fetching food items:", error);
+			return {
+				totalFoodItems: 0,
+				allFoodItems: [],
+			};
+		}
+	});
 }
 
 export type FoodItemsResult = Awaited<ReturnType<typeof getFoodItems>>;
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,14 @@
+import PageWrapper from "@/components/page-wrapper";
+import {H1} from "@/components/ui/typography";
+import Link from "next/link";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+	return (
+		<PageWrapper>
+			<H1>Home Page</H1>
+			<Link href="/food-items">
+				<span>Food Items</span>
+			</Link>
+		</PageWrapper>
+	);
 }
