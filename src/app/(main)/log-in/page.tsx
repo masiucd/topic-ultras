ee57b2b716a8f90573cd46628dfceb3b6dc0ff6b
masiucd <ciszekmarcell@gmail.com>
2024-10-20 18:30:03 +0200

    Refactor login and register pages

--- src/app/(main)/log-in/page.tsx
+++ src/app/(main)/log-in/page.tsx
@@ -1,8 +1,34 @@
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LoginPage() {
-  return (
-    <div>
-      <h1>Register</h1>
-      <p>Register for Nutri Check</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Register</h1>
+			<p>Log in for Nutri Check</p>
+			<div>
+				<form action="">
+					<fieldset>
+						<legend>Log in</legend>
+						<div>
+							<label htmlFor="email">Email</label>
+							<input type="email" id="email" name="email" />
+						</div>
+						<div>
+							<label htmlFor="password">Password</label>
+							<input type="password" id="password" name="password" />
+						</div>
+
+						<Button type="submit">Login</Button>
+					</fieldset>
+				</form>
+				<small>
+					Don't have an account? <Link href="/register">Register</Link>
+				</small>
+				<small>
+					<Link href="/forgot-password">Forgot password?</Link>
+				</small>
+			</div>
+		</div>
+	);
 }
--- src/app/(main)/register/page.tsx
+++ src/app/(main)/register/page.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+
 export default function RegisterPage() {
-  return (
-    <div>
-      <h1>Register</h1>
-      <p>Register for Nutri Check</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Register</h1>
+			<p>Register for Nutri Check</p>
+			<div>
+				<form action="">
+					<fieldset>
+						<legend>
+							<h2>Register</h2>
+						</legend>
+						<label htmlFor="username">Username</label>
+						<input type="text" id="username" name="username" />
+						<label htmlFor="password">Password</label>
+						<input type="password" id="password" name="password" />
+						<label htmlFor="confirmPassword">Confirm Password</label>
+						<input type="password" id="confirmPassword" name="confirmPassword" />
+						<button type="submit">Register</button>
+					</fieldset>
+				</form>
+				<small>
+					Already have an account? <Link href="/log-in">Log in</Link>
+				</small>
+			</div>
+		</div>
+	);
 }
