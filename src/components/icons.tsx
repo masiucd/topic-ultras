3cb8cdde74004d7759aa2bdb9c3290103ca7f5c2
masiucd <ciszekmarcell@gmail.com>
2024-10-20 17:48:59 +0200

    Refactor icons module and add new food icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,11 @@
 import {MoonIcon, SunIcon} from "@radix-ui/react-icons";
+import {Beef, Droplet, Flame, Zap} from "lucide-react";
 
 export let Icons = Object.freeze({
 	Moon: MoonIcon,
 	Sun: SunIcon,
+	Fat: Droplet,
+	Caloreis: Flame,
+	Protein: Beef,
+	Carbs: Zap,
 });
