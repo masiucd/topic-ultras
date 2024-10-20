d9799795d25abe654ba534f7cc5f2e644c5cd008
masiucd <ciszekmarcell@gmail.com>
2024-10-20 17:48:51 +0200

    Refactor food item page and remove unused code

--- src/app/(main)/food-items/[fooditem]/page.tsx
+++ src/app/(main)/food-items/[fooditem]/page.tsx
@@ -1,18 +1,9 @@
 import PageWrapper from "@/components/page-wrapper";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
 import {H1} from "@/components/ui/typography";
-import {db} from "@/db";
-import {foodCategories, foodItems, foodNutrients} from "@/db/schema";
 import {unSlugify} from "@/lib/utils";
-import {eq} from "drizzle-orm";
 import type {Metadata} from "next";
+import {FoodItem} from "./_components/food-item";
+import {getFoodItemData} from "./dao";
 
 type Props = {
 	params: {
@@ -33,43 +24,13 @@ export default async function FoodItemPage(props: Props) {
 	if (!foodItem) {
 		return <div>Food item not found</div>;
 	}
+
 	return (
 		<PageWrapper>
 			<H1>Food item {unSlugify(props.params.fooditem)}</H1>
-			<Card>
-				<CardHeader>
-					<CardTitle className="capitalize">{foodItem.name}</CardTitle>
-					<CardDescription>{foodItem.description}</CardDescription>
-				</CardHeader>
-				<CardContent>{/*  */}</CardContent>
-				<CardFooter>{/*  */}</CardFooter>
-			</Card>
+			<div className="my-5 flex w-[30rem] ">
+				<FoodItem foodItem={foodItem} />
+			</div>
 		</PageWrapper>
 	);
-}
-
-async function getFoodItemData(foodName: string) {
-	let data = await db
-		.select({
-			foodId: foodItems.id,
-			name: foodItems.name,
-			description: foodItems.description,
-			foodType: foodItems.foodType,
-			foodCategory: foodCategories.name,
-			nutrition: {
-				calories: foodNutrients.calories,
-				protein: foodNutrients.protein,
-				fat: foodNutrients.fat,
-				carbs: foodNutrients.carbs,
-			},
-		})
-		.from(foodItems)
-		.innerJoin(foodNutrients, eq(foodItems.id, foodNutrients.foodId))
-		.innerJoin(foodCategories, eq(foodItems.foodCategoryId, foodCategories.id))
-		.where(eq(foodItems.name, unSlugify(foodName)));
-
-	if (!data.length) {
-		return null;
-	}
-	return data[0];
 }
