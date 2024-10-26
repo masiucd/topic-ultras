445e43c2cc5076faafe3c44f7dd2264f9db89c00
marcell ciszek <ciszekmarcell@gmail.com>
2024-10-26 15:51:13 +0200

    Refactor next.config.mjs and tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true,
-  },
+  // experimental: {
+  //   typedRoutes: true,
+  // },
 };
 export default nextConfig;
