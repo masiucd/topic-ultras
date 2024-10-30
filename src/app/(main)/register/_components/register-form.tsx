a65adb6bf791304bdb416b25b5abaafdea40df4b
marcell ciszek <ciszekmarcell@gmail.com>
2024-10-30 14:53:16 +0100

    Refactor register form and validation handling; add error formatting utility

--- src/app/(main)/register/_components/register-form.tsx
+++ src/app/(main)/register/_components/register-form.tsx
@@ -7,67 +7,73 @@ import {cn} from "@/lib/utils";
 import {useActionState} from "react";
 
 export function RegisterForm() {
-	let [state, action, pending] = useActionState(register, null);
-	console.log("error", state);
-	return (
-		<form action={action}>
-			<fieldset
-				className={cn(
-					"flex flex-col gap-3 rounded-sm border-2 border-foreground p-4",
-					pending && "animate-pulse opacity-55",
-				)}
-			>
-				<legend>
-					<H3>Account Information</H3>
-				</legend>
-				<Label htmlFor="username">
-					<span>Username</span>
-					<Input
-						type="text"
-						id="username"
-						name="username"
-						defaultValue={state?.values.username}
-						required
-					/>
-					<span className="text-red-500">{state?.error?.get("username")?.message}</span>
-				</Label>
-				<Label htmlFor="email">
-					<span>Email</span>
-					<Input
-						type="email"
-						id="email"
-						name="email"
-						defaultValue={state?.values.email}
-						required
-					/>
-					<span className="text-red-500">{state?.error?.get("email")?.message}</span>
-				</Label>
-				<Label htmlFor="password">
-					<span>Password</span>
-					<Input
-						type="password"
-						id="password"
-						name="password"
-						defaultValue={state?.values.password}
-						required
-					/>
-					<span className="text-red-500">{state?.error?.get("password")?.message}</span>
-				</Label>
-				<Label htmlFor="confirmPassword">
-					<span>Confirm Password</span>
-					<Input
-						type="password"
-						id="confirmPassword"
-						name="confirmPassword"
-						defaultValue={state?.values.confirmPassword}
-						required
-					/>
-					<span className="text-red-500">
-						{state?.error?.get("confirmPassword")?.message}
-					</span>
-				</Label>
-				<Button type="submit">Register</Button>
-			</fieldset>
-		</form>
-	);
+  let [state, action, pending] = useActionState(register, null);
+  console.log("error", state?.error, state?.error?.get("password"));
+  return (
+    <form action={action}>
+      <fieldset
+        className={cn(
+          "flex flex-col gap-3 rounded-sm border-2 border-foreground p-4",
+          pending && "animate-pulse opacity-55"
+        )}
+      >
+        <legend>
+          <H3>Account Information</H3>
+        </legend>
+        <Label htmlFor="username">
+          <span>Username</span>
+          <Input
+            type="text"
+            id="username"
+            name="username"
+            defaultValue={state?.values.username}
+            required
+          />
+          <span className="text-red-500">
+            {state?.error?.get("username")?.message}
+          </span>
+        </Label>
+        <Label htmlFor="email">
+          <span>Email</span>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            defaultValue={state?.values.email}
+            required
+          />
+          <span className="text-red-500">
+            {state?.error?.get("email")?.message}
+          </span>
+        </Label>
+        <Label htmlFor="password">
+          <span>Password</span>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            defaultValue={state?.values.password}
+            required
+          />
+          <span className="text-red-500">
+            {state?.error?.get("password")?.message}
+          </span>
+        </Label>
+        <Label htmlFor="confirmPassword">
+          <span>Confirm Password</span>
+          <Input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            defaultValue={state?.values.confirmPassword}
+            required
+          />
+          <span className="text-red-500">
+            {state?.error?.get("confirmPassword")?.message}
+          </span>
+        </Label>
+        <Button type="submit">Register</Button>
+      </fieldset>
+    </form>
+  );
 }
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,65 +1,79 @@
 "use server";
 import "server-only";
+import {formatErrors, formatZodErrors} from "@/lib/helpers";
 import {z} from "zod";
 
 let registerSchema = z.object({
-	username: z.string().min(3),
-	email: z.string().email(),
-	password: z.string().min(6),
-	confirmPassword: z.string().min(6),
+  username: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(6),
+  confirmPassword: z.string().min(6),
 });
+
+type ErrosObj = Map<
+  string,
+  {
+    message: string;
+  }
+>;
 
 type RegisterSchema = z.infer<typeof registerSchema>;
 type PrevState = {
-	values: RegisterSchema;
-	error: ReturnType<typeof formatErrors> | null;
+  values: RegisterSchema;
+  // error: ReturnType<typeof formatZodErrors> | null;
+  error: ErrosObj | null;
 };
 
+let RegisterInputNames = Object.freeze({
+  username: "username",
+  email: "email",
+  password: "password",
+  confirmPassword: "confirmPassword",
+});
+
 export async function register(
-	_prevState: PrevState | null,
-	data: FormData,
+  _prevState: PrevState | null,
+  data: FormData
 ): Promise<PrevState> {
-	let formValues = Object.fromEntries(data);
-	let validatedData = registerSchema.safeParse(formValues);
-	if (!validatedData.success) {
-		let error = formatErrors(validatedData.error.errors);
-		console.log("validatedData.error.errors", validatedData.error.errors);
+  let formValues = Object.fromEntries(data);
+  let validatedData = registerSchema.safeParse(formValues);
+  if (!validatedData.success) {
+    return {
+      values: {
+        [RegisterInputNames.username]: (formValues.username as string) ?? "",
+        [RegisterInputNames.email]: (formValues.email as string) ?? "",
+        [RegisterInputNames.password]: (formValues.password as string) ?? "",
+        [RegisterInputNames.confirmPassword]:
+          (formValues.confirmPassword as string) ?? "",
+      },
+      error: formatZodErrors(validatedData.error.errors),
+    };
+  }
+  let {username, email, password, confirmPassword} = validatedData.data;
 
-		return {
-			values: {
-				username: (formValues.username as string) ?? "",
-				email: (formValues.email as string) ?? "",
-				password: (formValues.password as string) ?? "",
-				confirmPassword: (formValues.confirmPassword as string) ?? "",
-			},
-			error,
-		};
-	}
-	let {username, email, password, confirmPassword} = validatedData.data;
-	return {
-		values: {username, email, password, confirmPassword},
-		error: null,
-	};
-}
+  if (password !== confirmPassword) {
+    return {
+      values: {
+        username: (formValues.username as string) ?? "",
+        email: (formValues.email as string) ?? "",
+        password: (formValues.password as string) ?? "",
+        confirmPassword: (formValues.confirmPassword as string) ?? "",
+      },
+      error: formatErrors([
+        {
+          message: "Password does not match",
+          name: RegisterInputNames.password,
+        },
+        {
+          message: "Password does not match",
+          name: RegisterInputNames.confirmPassword,
+        },
+      ]),
+    };
+  }
 
-/**
- * Formats an array of Zod issues into a Map where the keys are the error paths and the values are objects containing the error message and code.
- *
- * @param errors - An array of Zod issues to format.
- * @returns A Map where each key is an error path and each value is an object containing the error message and code.
- */
-function formatErrors(errors: z.ZodIssue[]) {
-	let result = new Map<string, {message: string; code: string}>();
-	for (let error of errors) {
-		let obj = {
-			message: error.message.replace("String", pathToString(error.path)),
-			code: error.code,
-		};
-		result.set(pathToString(error.path), obj);
-	}
-	return result;
-}
-
-function pathToString(path: (string | number)[]) {
-	return typeof path[0] === "string" ? path[0] : typeof path[0].toString();
+  return {
+    values: {username, email, password, confirmPassword},
+    error: null,
+  };
 }
