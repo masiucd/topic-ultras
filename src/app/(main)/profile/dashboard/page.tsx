d26835392ea1d8120cc449dc51f0f8ee2cefceca
marcell ciszek <ciszekmarcell@gmail.com>
2024-10-31 13:01:15 +0100

    Implement protected route for Dashboard page; add session validation and redirect for unauthenticated users

--- src/app/(main)/profile/dashboard/page.tsx
+++ src/app/(main)/profile/dashboard/page.tsx
@@ -1,8 +1,42 @@
-export default function DashboardPage() {
+import PageWrapper from "@/components/page-wrapper";
+import {H1} from "@/components/ui/typography";
+import {getUserByEmail} from "@/db/dao/user";
+import {decrypt} from "@/lib/jwt";
+
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+
+// Protected route
+
+async function isLoggedIn() {
+  let cookieStore = await cookies();
+  let session = cookieStore.get("session");
+  if (!session) {
+    return false;
+  }
+  // Decrypt session
+  let decrypted = await decrypt(session.value);
+  if (decrypted === null) {
+    return false;
+  }
+  // TODO using redis somehow
+  let user = await getUserByEmail(decrypted.email);
+  if (user === null) {
+    return false;
+  }
+
+  return true;
+}
+
+export default async function DashboardPage() {
+  let loggedIn = await isLoggedIn();
+  if (loggedIn) {
+    redirect("/log-in");
+  }
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <PageWrapper>
+      <H1>Dashboard</H1>
       <p>Dashboard page</p>
-    </div>
+    </PageWrapper>
   );
 }
