c9809590ffe824c02e09e92632c0f6bab06213f4
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-11 17:18:56 +0100

    Add Button and Typography components: implement reusable Button component with variants and sizes; create H1, H2, H3, H4, and other typography components for consistent styling; update root and index routes to utilize new components.

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type {LinksFunction} from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,12 +6,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {rel: "preconnect", href: "https://fonts.googleapis.com"},
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
@@ -22,7 +22,9 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({children}: {children: React.ReactNode}) {
+  let x = false;
+  console.log(x);
   return (
     <html lang="en">
       <head>
---
+++ app/routes/sign-in/route.tsx
@@ -0,0 +1,9 @@
+import {H1} from "~/components/ui/typography";
+
+export default function Signing() {
+  return (
+    <div>
+      <H1>Sign in</H1>
+    </div>
+  );
+}
