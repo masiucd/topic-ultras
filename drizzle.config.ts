525395729e18b01dc50d6897be6d0ab606ec1b55
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-11 17:30:54 +0100

    Add environment variable configuration with validation and update Drizzle config import

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,14 @@
 import {defineConfig} from "drizzle-kit";
-
-import env from "@/env";
+import {env} from "./env";
 
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/db/schema/index.ts",
-  out: "./src/db/migrations",
-  dbCredentials: {
-    url: env.DB_URL,
-  },
-  breakpoints: true,
-  verbose: env.NODE_ENV === "development", // Log all queries in development
-  strict: true, // Enable strict mode
+	dialect: "postgresql",
+	schema: "./src/db/schema/index.ts",
+	out: "./src/db/migrations",
+	dbCredentials: {
+		url: env.DB_URL,
+	},
+	breakpoints: true,
+	verbose: env.NODE_ENV === "development", // Log all queries in development
+	strict: true, // Enable strict mode
 });
-
-
-
---
+++ env.ts
@@ -0,0 +1,35 @@
+import {config} from "dotenv";
+import {expand} from "dotenv-expand";
+import {z} from "zod";
+
+let stringBoolean = z.coerce
+	.string()
+	.transform((v) => v === "true")
+	.default("false");
+
+let envSchema = z.object({
+	NODE_ENV: z.string(),
+	DB_HOST: z.string(),
+	DB_USER: z.string(),
+	DB_PASSWORD: z.string(),
+	DB_NAME: z.string(),
+	DB_PORT: z.string(),
+	REDIS_PORT: z.string(),
+	DB_URL: z.string(),
+	JWT_SECRET: z.string(),
+	DB_MIGRATION: stringBoolean,
+	DB_SEED: stringBoolean,
+});
+
+// Load environment variables
+expand(config());
+
+export type Env = z.infer<typeof envSchema>;
+
+let {success, data, error} = envSchema.safeParse(process.env);
+if (!success) {
+	console.error(error?.errors);
+	process.exit(1);
+}
+
+export const env = data as Env;
