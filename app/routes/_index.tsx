ff4a268c0664675e47c802ac864dc46f55c2297e
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-12 09:15:54 +0100

    Implement data loader for user retrieval in index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type {MetaFunction} from "@remix-run/node";
+import {useLoaderData} from "@remix-run/react";
+import {db} from "~/.server/db";
+import {users} from "~/.server/db/schema";
 import {H1} from "~/components/ui/typography";
 
 export const meta: MetaFunction = () => {
@@ -8,7 +11,14 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  let result = await db.select().from(users);
+  return result;
+}
+
 export default function Index() {
+  let data = useLoaderData<typeof loader>();
+  console.log("data", data);
   return (
     <div className="flex h-screen items-center justify-center">
       <H1>Welcome to Nutri Check</H1>
