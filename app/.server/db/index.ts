79f0bde1ea8d383932dbad52940e5d2811b53291
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-12 09:04:57 +0100

    Enhance user schema with detailed fields and unique index; refactor badge component for cleaner code

--- app/.server/db/index.ts
+++ app/.server/db/index.ts
@@ -6,12 +6,11 @@ import * as schema from "./schema";
 
 export let connection = postgres(env.DB_URL, {
 	max: env.DB_MIGRATION || env.DB_SEED ? 1 : undefined, // Limit the number of connections to 1 during migrations and seeding
-	// eslint-disable-next-line no-console
 	onnotice: env.DB_SEED ? console.log : undefined, // Log notices during seeding
 });
 
 export let db = drizzle(connection, {
-	schema, // All tables will be added to the schema
+	schema,
 	logger: env.NODE_ENV === "development", // Log all queries in development
 });
 
--- app/.server/db/schema/users.ts
+++ app/.server/db/schema/users.ts
@@ -1,3 +1,19 @@
-import {pgTable} from "drizzle-orm/pg-core";
+import {pgTable, serial, timestamp, uniqueIndex, varchar} from "drizzle-orm/pg-core";
 
-export let users = pgTable("users", {});
+export let users = pgTable(
+	"users",
+	{
+		id: serial("id").primaryKey().notNull(),
+		firstName: varchar("first_name", {length: 100}),
+		lastName: varchar("last_name", {length: 100}),
+		email: varchar("email", {length: 120}).notNull(),
+		password: varchar("password", {length: 200}).notNull(),
+		createdAt: timestamp("created_at", {mode: "string"}).defaultNow(),
+		updatedAt: timestamp("updated_at", {mode: "string"})
+			.defaultNow()
+			.$onUpdate(() => new Date().toISOString()),
+	},
+	(table) => ({
+		emailIndex: uniqueIndex("user_email_index").on(table.email),
+	}),
+);
