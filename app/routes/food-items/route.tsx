c365114e323e7721f178268bfe26684c7d3dfc41
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-13 14:26:08 +0100

    Implement pagination for food items with navigation links and update data loading logic

--- app/routes/food-items/route.tsx
+++ app/routes/food-items/route.tsx
@@ -1,5 +1,5 @@
 import type {LoaderFunctionArgs} from "@remix-run/node";
-import {useLoaderData, useLocation, useNavigate} from "@remix-run/react";
+import {Link, useLoaderData, useLocation, useNavigate} from "@remix-run/react";
 import {eq, ilike, sql} from "drizzle-orm";
 import {db} from "~/.server/db";
 import {
@@ -9,28 +9,48 @@ import {
   slugs,
 } from "~/.server/db/schema";
 import {Input} from "~/components/ui/input";
+// import {
+//   Pagination,
+//   PaginationContent,
+//   PaginationEllipsis,
+//   PaginationItem,
+//   PaginationLink,
+//   PaginationNext,
+//   PaginationPrevious,
+// } from "~/components/ui/pagination";
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
 import {H1} from "~/components/ui/typography";
+
 // import  invariant from "tiny-invariant";
 
 export async function loader({request}: LoaderFunctionArgs) {
   let url = new URL(request.url);
   let name = url.searchParams.get("name");
-  let limit = Number.parseInt(url.searchParams.get("limit") ?? "4", 10);
-  let skip = Number.parseInt(url.searchParams.get("skip") ?? "0", 10);
-  console.log({name, limit, skip});
+  // let limit = Number.parseInt(url.searchParams.get("limit") ?? "4", 10);
+  // let skip = Number.parseInt(url.searchParams.get("skip") ?? "0", 10);
+  let perPage = 4;
+  let page = url.searchParams.get("page");
 
-  // let limit = 4;
-  // let skip = 0;
   try {
+    let totalUsers = await db
+      .select({count: sql`count(*)`.mapWith(Number)})
+      .from(foodItems);
+
+    let totalPages = Math.ceil(totalUsers[0].count / perPage);
+    let pageFromUrl =
+      typeof page === "string" && Number.parseInt(page, 10) <= totalPages
+        ? Number.parseInt(page, 10)
+        : 1;
+
     let results = await db
       .select({
         foodId: foodItems.id,
@@ -54,18 +74,26 @@ export async function loader({request}: LoaderFunctionArgs) {
       .leftJoin(slugs, eq(foodItems.id, slugs.objectId))
       .orderBy(sql`${foodItems.name} ASC`)
       .where(name ? ilike(foodItems.name, `%${name}%`) : undefined)
-      .limit(limit)
-      .offset(skip);
+      .limit(perPage)
+      .offset((pageFromUrl - 1) * perPage);
 
-    return results;
+    return {
+      results,
+      page: pageFromUrl,
+      totalPages,
+    };
   } catch (error) {
     console.error(error);
-    return [];
+    return {
+      results: [],
+      page: 1,
+      totalPages: 1,
+    };
   }
 }
 
 export default function FoodItemsRoute() {
-  let results = useLoaderData<typeof loader>();
+  let {results} = useLoaderData<typeof loader>();
 
   return (
     <div>
@@ -102,6 +130,14 @@ export default function FoodItemsRoute() {
               </TableRow>
             ))}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={7}>Total</TableCell>
+              <TableCell className="">
+                <Navigation />
+              </TableCell>
+            </TableRow>
+          </TableFooter>
         </Table>
       </div>
     </div>
@@ -109,7 +145,6 @@ export default function FoodItemsRoute() {
 }
 
 function SearchInput() {
-  // let [s] = useSearchParams();
   let navigate = useNavigate();
   let location = useLocation();
   return (
@@ -119,7 +154,9 @@ function SearchInput() {
       onChange={(e) => {
         let search = new URLSearchParams(location.search);
         search.set("name", e.target.value);
-        console.log("search", search.get("name"));
+        if (search.get("page")) {
+          search.delete("page");
+        }
         navigate(`${location.pathname}?${search.toString()}`);
         if (e.target.value === "") {
           search.delete("name");
@@ -128,4 +165,94 @@ function SearchInput() {
       }}
     />
   );
+}
+
+// function TableNavigation() {
+//   return (
+//     <Pagination>
+//       <PaginationContent>
+//         <PaginationItem>
+//           <PaginationPrevious href="#" />
+//         </PaginationItem>
+//         <PaginationItem>
+//           <PaginationLink href="#">1</PaginationLink>
+//         </PaginationItem>
+//         <PaginationItem>
+//           <PaginationEllipsis />
+//         </PaginationItem>
+//         <PaginationItem>
+//           <PaginationNext href="#" />
+//         </PaginationItem>
+//       </PaginationContent>
+//     </Pagination>
+//   );
+// }
+
+function Navigation() {
+  let location = useLocation();
+
+  let search = new URLSearchParams(location.search);
+  console.log("search.toString()", search.toString());
+  let page = Number.parseInt(search.get("page") ?? "1", 10);
+
+  let prevLinkPath =
+    page !== null && page > 2
+      ? `${location.pathname}?page=${page - 1}`
+      : location.pathname;
+
+  let nextLinkPath =
+    page !== null ? `${location.pathname}?page=${page + 1}` : location.pathname;
+
+  console.log("🚀 ~ Navigation ~ prevLinkPath:", prevLinkPath);
+  console.log("page", page);
+
+  return (
+    <div className="flex gap-2">
+      {/* <Link
+        to={prevLinkPath}
+        // className={cn("", page <= 1 ? "pointer-events-none text-gray-400" : "")}
+      >
+        Prev
+      </Link> */}
+      <PreviousLink />
+      <NextLink />
+      {/* <Link to={nextLinkPath}>Next</Link> */}
+    </div>
+  );
+}
+
+function PreviousLink() {
+  let location = useLocation();
+  let search = new URLSearchParams(location.search);
+  if (search.get("name")) {
+    search.delete("name");
+  }
+  let page = Number.parseInt(search.get("page") ?? "1", 10);
+  if (page < 2) {
+    console.log("RENDER BUTTON");
+    return (
+      <button
+        className="opacity-50"
+        disabled
+        aria-disabled="true"
+        type="button"
+      >
+        Prev
+      </button>
+    );
+  }
+  let to =
+    page > 2 ? `${location.pathname}?page=${page - 1}` : location.pathname;
+  return <Link to={to}>Prev</Link>;
+}
+
+function NextLink() {
+  let location = useLocation();
+  let search = new URLSearchParams(location.search);
+  if (search.get("name")) {
+    search.delete("name");
+  }
+  let page = Number.parseInt(search.get("page") ?? "1", 10);
+  let to = `${location.pathname}?page=${page + 1}`;
+  return <Link to={to}>Next</Link>;
 }
