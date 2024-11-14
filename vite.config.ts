ea8406c786a2951aa1fba7594322a81805c8e4e0
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-14 14:59:15 +0100

    Add Radix UI dependencies and clean up Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -15,15 +15,6 @@ export default defineConfig({
 		globals: true,
 	},
 	plugins: [
-		// remix({
-		// 	future: {
-		// 		v3_fetcherPersist: true,
-		// 		v3_relativeSplatPath: true,
-		// 		v3_throwAbortReason: true,
-		// 		v3_singleFetch: true,
-		// 		v3_lazyRouteDiscovery: true,
-		// 	},
-		// }),
 		process.env.NODE_ENV === "test"
 			? null
 			: remix({
