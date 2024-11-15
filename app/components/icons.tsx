04402235623c319fa2602690dc376d04234e2935
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-15 17:22:25 +0100

    Add icon components for navigation arrows using lucide-react

---
+++ app/components/icons.tsx
@@ -0,0 +1,8 @@
+import {ArrowBigLeft, ArrowBigRight, ArrowLeft, ArrowRight} from "lucide-react";
+
+export const Icons = Object.freeze({
+  RightArrow: ArrowRight,
+  LeftArrow: ArrowLeft,
+  BigRightArrow: ArrowBigRight,
+  BigLeftArrow: ArrowBigLeft,
+});
