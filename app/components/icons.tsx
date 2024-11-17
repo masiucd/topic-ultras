0547c034d43675527d8213bc7c0a5cb817ba7398
masiucd <ciszekmarcell@gmail.com>
2024-11-17 16:28:00 +0100

    Refactor icon components to accept props and standardize icon size

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -7,17 +7,34 @@ import {
   ChevronRight,
   ChevronsLeft,
   ChevronsRight,
+  type LucideProps,
   Tag,
 } from "lucide-react";
 
+export const ICON_SIZE = 16;
+
 export const Icons = Object.freeze({
-  RightArrow: ArrowRight,
-  LeftArrow: ArrowLeft,
-  BigRightArrow: ArrowBigRight,
-  BigLeftArrow: ArrowBigLeft,
-  ChevronRight,
-  ChevronLeft,
-  ChevronsLeft,
-  ChevronsRight,
-  Category: Tag,
+  RightArrow: (props: LucideProps) => (
+    <ArrowRight size={ICON_SIZE} {...props} />
+  ),
+  LeftArrow: (props: LucideProps) => <ArrowLeft size={ICON_SIZE} {...props} />,
+  BigRightArrow: (props: LucideProps) => (
+    <ArrowBigRight size={ICON_SIZE} {...props} />
+  ),
+  BigLeftArrow: (props: LucideProps) => (
+    <ArrowBigLeft size={ICON_SIZE} {...props} />
+  ),
+  ChevronRight: (props: LucideProps) => (
+    <ChevronRight size={ICON_SIZE} {...props} />
+  ),
+  ChevronLeft: (props: LucideProps) => (
+    <ChevronLeft size={ICON_SIZE} {...props} />
+  ),
+  ChevronsLeft: (props: LucideProps) => (
+    <ChevronsLeft size={ICON_SIZE} {...props} />
+  ),
+  ChevronsRight: (props: LucideProps) => (
+    <ChevronsRight size={ICON_SIZE} {...props} />
+  ),
+  Category: (props: LucideProps) => <Tag size={ICON_SIZE} {...props} />,
 });
