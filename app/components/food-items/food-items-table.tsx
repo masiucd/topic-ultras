b8ea5d18915d8604bcf755dd3f9bbd7aec215897
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-22 17:06:20 +0100

    Refactor FoodItemsTable and Pagination components to integrate Tooltip for navigation and remove unused SelectRows functionality

--- app/components/food-items/food-items-table.tsx
+++ app/components/food-items/food-items-table.tsx
@@ -1,4 +1,4 @@
-import {Link, type Location, useSearchParams} from "@remix-run/react";
+import {Link, type Location} from "@remix-run/react";
 import {useState} from "react";
 import type {FoodItemData} from "~/.server/db/dao/food-items";
 import {FoodCategory} from "~/components/food-category";
@@ -6,23 +6,14 @@ import {Checkbox} from "~/components/ui/checkbox";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
-import {DEFAULT_FOOD_ITEMS_ROWS} from "~/lib/constants";
 import {cn} from "~/lib/utils";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
-import {Span} from "../ui/typography";
+import {SelectRows} from "./filters/rows";
 import {Pagination} from "./pagination";
 
 export function FoodItems(props: {
@@ -39,7 +30,6 @@ export function FoodItems(props: {
   );
   return (
     <Table title="Food items table">
-      <TableCaption>Food Items in the Database.</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[10px]">
@@ -152,10 +142,7 @@ function Footer(props: {
         </TableCell>
         <TableCell colSpan={5}>
           <div className="flex items-center ">
-            <div className="mr-3 flex flex-1 items-center justify-end gap-3 ">
-              <Span>Rows per page</Span>
-              <SelectRows totalFoodItems={totalFoodItems} />
-            </div>
+            <SelectRows totalFoodItems={totalFoodItems} />
             <Pagination
               page={page}
               location={location}
@@ -166,40 +153,4 @@ function Footer(props: {
       </TableRow>
     </TableFooter>
   );
-}
-
-function SelectRows(props: {totalFoodItems: number}) {
-  let [searchParams, setSearchParams] = useSearchParams();
-  let rows = searchParams.get("rows") ?? DEFAULT_FOOD_ITEMS_ROWS;
-  return (
-    <Select
-      name="rows"
-      onValueChange={(value) => {
-        let params = new URLSearchParams(searchParams);
-        let rows = params.get("rows");
-        console.log("🚀 ~ SelectRows ~ rows:", rows);
-        params.set("rows", value);
-        setSearchParams(params, {preventScrollReset: true});
-      }}
-    >
-      <SelectTrigger className="h-7 w-[60px]">
-        <SelectValue placeholder={rows} />
-      </SelectTrigger>
-      <SelectContent>
-        {makeRowValues(props.totalFoodItems).map((p) => (
-          <SelectItem key={p} value={p.toString()}>
-            {p}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  );
-}
-
-function makeRowValues(totalFoodItems: number) {
-  let xs = [];
-  for (let i = 2; i < totalFoodItems; i += 2) {
-    xs.push(i);
-  }
-  return xs;
 }
--- app/components/food-items/pagination.tsx
+++ app/components/food-items/pagination.tsx
@@ -1,6 +1,7 @@
 import {Link, type Location} from "@remix-run/react";
 import {cn} from "~/lib/utils";
 import {Icons} from "../icons";
+import {TooltipComponent} from "../ui/tooltip";
 
 export function Pagination(props: {
   page: number;
@@ -9,27 +10,40 @@ export function Pagination(props: {
 }) {
   return (
     <div className="flex gap-2">
-      <Link
-        to={`${props.location.pathname}?page=1`}
-        className={cn("", props.page === 1 && "pointer-events-none opacity-50")}
+      <TooltipComponent
+        content="Go to the first page"
+        disabled={props.page === 1}
       >
-        <Icons.ChevronsLeft />
-      </Link>
+        <Link
+          to={`${props.location.pathname}?page=1`}
+          className={cn(
+            "",
+            props.page === 1 && "pointer-events-none opacity-50"
+          )}
+        >
+          <Icons.ChevronsLeft />
+        </Link>
+      </TooltipComponent>
       <PreviousLink page={props.page} location={props.location} />
       <NextLink
         page={props.page}
         totalPages={props.totalPages}
         location={props.location}
       />
-      <Link
-        to={`${props.location.pathname}?page=${props.totalPages}`}
-        className={cn(
-          "",
-          props.page === props.totalPages && "pointer-events-none opacity-50"
-        )}
+      <TooltipComponent
+        content="Go to the last page"
+        disabled={props.page === props.totalPages}
       >
-        <Icons.ChevronsRight />
-      </Link>
+        <Link
+          to={`${props.location.pathname}?page=${props.totalPages}`}
+          className={cn(
+            "",
+            props.page === props.totalPages && "pointer-events-none opacity-50"
+          )}
+        >
+          <Icons.ChevronsRight />
+        </Link>
+      </TooltipComponent>
     </div>
   );
 }
@@ -48,13 +62,21 @@ function PreviousLink(props: {page: number; location: Location}) {
   let url = `${location.pathname}?${searchParams.toString()}`;
   let isDisabled = page < 2;
   return (
-    <Link
-      to={url}
-      className={cn("", isDisabled && "pointer-events-none opacity-50")}
-      aria-disabled={isDisabled}
+    <TooltipComponent
+      content={
+        props.page === 1
+          ? "You are on the first page"
+          : "Go to the previous page"
+      }
     >
-      <Icons.ChevronLeft />
-    </Link>
+      <Link
+        to={url}
+        className={cn("", isDisabled && "pointer-events-none opacity-50")}
+        aria-disabled={isDisabled}
+      >
+        <Icons.ChevronLeft />
+      </Link>
+    </TooltipComponent>
   );
   // return <Link to={url}>Prev</Link>;
 }
@@ -71,11 +93,19 @@ function NextLink(props: {
   searchParams.set("page", (page + 1).toString());
   let url = `${location.pathname}?${searchParams.toString()}`;
   return (
-    <Link
-      to={url}
-      className={cn("", isDisabled && "pointer-events-none opacity-50")}
+    <TooltipComponent
+      content={
+        props.page === props.totalPages
+          ? "You are on the last page"
+          : "Go to the next page"
+      }
     >
-      <Icons.ChevronRight />
-    </Link>
+      <Link
+        to={url}
+        className={cn("", isDisabled && "pointer-events-none opacity-50")}
+      >
+        <Icons.ChevronRight />
+      </Link>
+    </TooltipComponent>
   );
 }
