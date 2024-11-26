956702ce11ba703f8b85fd2442df36da9b0c1158
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-26 12:10:18 +0100

    reset

--- src/app/(main)/foods/food-types/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function FoodTypesPage() {
-  return <div>FoodTypesPage</div>;
-}
--- src/db/migrations/0001_smooth_captain_universe.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "sessions" ADD COLUMN "expires" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "sessions" DROP COLUMN IF EXISTS "expires_at";
