4f4db32b0cc859d6a9608670ef4015d489d54d96
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-27 09:25:34 +0100

    feat: Add route for food items in routes configuration

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,12 @@
-import {type RouteConfig, index, route} from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  // layout
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("about", "routes/about.tsx"),
+  route("food-items", "routes/food-items.tsx"),
 ] satisfies RouteConfig;
