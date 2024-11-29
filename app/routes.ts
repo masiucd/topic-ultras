5ddc6cd5541f98db1885e26f52a60654eb3a6ada
marcell ciszek <ciszekmarcell@gmail.com>
2024-11-29 08:48:52 +0100

    feat: Add authentication layout and routes for login and registration

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,12 @@
-import {
-  type RouteConfig,
-  index,
-  route,
-  // layout
-} from "@react-router/dev/routes";
+import {type RouteConfig, index, layout, route} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("about", "routes/about.tsx"),
   route("food-items", "routes/food-items.tsx"),
+  layout("routes/_auth.tsx", [
+    route("login", "routes/_auth.login.tsx"),
+    route("register", "routes/_auth.register.tsx"),
+  ]),
+  // route("register", "routes/_auth.register.tsx"),
 ] satisfies RouteConfig;
