0c3d690a79d4e52763b1cdd3f18f92e7dc50f57a
marcell ciszek <ciszekmarcell@gmail.com>
2024-12-07 11:47:13 +0100

    feat: Update color variables in CSS and add FoodData component with pie chart visualization

--- app/routes/food-item.tsx
+++ app/routes/food-item.tsx
@@ -109,18 +109,7 @@ function FoodCard({foodItem}: {foodItem: NonNullable<FoodItem>}) {
       </CardHeader>
       <CardContent>
         <div className="grid w-full items-center gap-4">
-          {/* <Label htmlFor="framework">Framework</Label> */}
-          {/* <Select> */}
-          {/* <SelectTrigger id="framework">
-              <SelectValue placeholder="Select" />
-            </SelectTrigger> */}
-          {/* <SelectContent position="popper">
-              <SelectItem value="next">Next.js</SelectItem>
-              <SelectItem value="sveltekit">SvelteKit</SelectItem>
-              <SelectItem value="astro">Astro</SelectItem>
-              <SelectItem value="nuxt">Nuxt.js</SelectItem>
-            </SelectContent> */}
-          {/* </Select> */}
+          <FoodData />
         </div>
       </CardContent>
       <CardFooter className="flex justify-between">
@@ -130,6 +119,125 @@ function FoodCard({foodItem}: {foodItem: NonNullable<FoodItem>}) {
           Edit{" "}
         </Button>
         {/* <Button>Deploy</Button> */}
+      </CardFooter>
+    </Card>
+  );
+}
+
+("use client");
+
+import {TrendingUp} from "lucide-react";
+import * as React from "react";
+import {Label, Pie, PieChart} from "recharts";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "~/components/ui/chart";
+
+const CHART_DATA = [
+  {browser: "chrome", visitors: 275, fill: "var(--color-chrome)"},
+  {browser: "safari", visitors: 200, fill: "var(--color-safari)"},
+  {browser: "firefox", visitors: 287, fill: "var(--color-firefox)"},
+  {browser: "edge", visitors: 173, fill: "var(--color-edge)"},
+  {browser: "other", visitors: 190, fill: "var(--color-other)"},
+];
+
+const CHART_CONFIG = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
+export function FoodData() {
+  const totalVisitors = React.useMemo(() => {
+    return CHART_DATA.reduce((acc, curr) => acc + curr.visitors, 0);
+  }, []);
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Pie Chart - Donut with Text</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={CHART_CONFIG}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={CHART_DATA}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({viewBox}) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground font-bold text-3xl"
+                        >
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Visitors
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="text-muted-foreground leading-none">
+          Showing total visitors for the last 6 months
+        </div>
       </CardFooter>
     </Card>
   );
