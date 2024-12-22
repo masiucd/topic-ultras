2ea192e23ced9034553087aed1d554761533688b
masiucd <ciszekmarcell@gmail.com>
2024-12-22 17:25:54 +0100

    feat: Replace password hashing with argon2 for improved security

--- app/.server/utils/password.ts
+++ app/.server/utils/password.ts
@@ -1,19 +1,22 @@
-import {createHash, timingSafeEqual} from "node:crypto";
+import {hash, verify} from "argon2";
 
-export async function hashPassword(password: string): Promise<string> {
-  const hash = createHash("sha256");
-  hash.update(password);
-  return hash.digest("hex");
+export async function hashPassword(password: string) {
+  try {
+    return await hash(password);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 export async function comparePassword(
   password: string,
   hash: string
 ): Promise<boolean> {
-  const passwordHash = createHash("sha256");
-  passwordHash.update(password);
-  const passwordDigest = passwordHash.digest();
-
-  const hashBuffer = Buffer.from(hash, "hex");
-  return timingSafeEqual(passwordDigest, hashBuffer);
+  try {
+    return await verify(hash, password);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
