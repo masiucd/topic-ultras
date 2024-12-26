d65e3e9db65111ed6b188080954ca7f5dd3550b6
masiucd <ciszekmarcell@gmail.com>
2024-12-26 17:11:54 +0100

    feat: Enhance email sending functionality with Redis token expiration and improved error handling

--- app/lib/send-email.ts
+++ app/lib/send-email.ts
@@ -1,19 +1,31 @@
 import fs from "node:fs/promises";
 import {env} from "$/env";
 import {Resend} from "resend";
+import {setExpire} from "~/.server/db/redis";
 import {encrypt} from "./jwt/email-token";
 
 export async function sendEmail(email: string) {
-  let file = await getMailTemplateAsHtmlString();
   let token = await encrypt(email);
+  let ok = await setExpire({
+    key: token,
+    value: email,
+    hours: 1,
+  });
 
-  let html =
-    file !== null
-      ? file.replaceAll(
-          "{{ url }}",
-          `http://localhost:4000/reset-password?token=${token}`
-        )
-      : "";
+  if (ok !== "OK") {
+    return {
+      data: null,
+      error: "Internal server error",
+    };
+  }
+
+  let html = await setUrlForHtmlTemplate(token);
+  if (html === null) {
+    return {
+      data: null,
+      error: "Internal server error",
+    };
+  }
   const resend = new Resend(env.RESEND_API_KEY);
   const {data, error} = await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -36,4 +48,15 @@ async function getMailTemplateAsHtmlString() {
     console.error("error", error);
     return null;
   }
+}
+
+async function setUrlForHtmlTemplate(token: string) {
+  let file = await getMailTemplateAsHtmlString();
+  if (file !== null) {
+    return file.replaceAll(
+      "{{ url }}",
+      `http://localhost:4000/reset-password?token=${token}`
+    );
+  }
+  return null;
 }
