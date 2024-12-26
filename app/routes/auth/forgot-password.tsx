79157a459552cc59d686d59bdf61062e21c8ae73
masiucd <ciszekmarcell@gmail.com>
2024-12-26 16:27:44 +0100

    feat: Enhance password reset flow with user validation and improved UI

--- app/routes/auth/forgot-password.tsx
+++ app/routes/auth/forgot-password.tsx
@@ -1,9 +1,13 @@
 import {Form} from "react-router";
 import {z} from "zod";
+import {getUserByEmail} from "~/.server/db/dao/users";
+import PageWrapper from "~/components/page-wrapper";
+import {Button} from "~/components/ui/button";
 import {Input} from "~/components/ui/input";
-import {Label} from "~/components/ui/typography";
+import {H1, H2, Label, Lead} from "~/components/ui/typography";
 import {sendEmail} from "~/lib/send-email";
 import type {Route} from "./+types/forgot-password";
+
 export function meta() {
   return [
     {
@@ -20,44 +24,54 @@ let ResetPasswordSchema = z.object({
 export async function action({request}: Route.ActionArgs) {
   let body = await request.formData();
   let {email} = ResetPasswordSchema.parse({email: body.get("email")});
-
-  // TODO genereate a token and store it in redis
-  // send the token in the email
-  // user clicks on the link and we verify the token
-  // if the token is valid, we allow the user to reset the password
-  // we check the token in route /reset-password
-
-  let {error} = await sendEmail(email);
-  if (error) {
-    console.error("error", error);
-    return {status: 500, data: {error: "Internal server error"}};
+  let user = await getUserByEmail(email);
+  if (user !== null) {
+    let {error} = await sendEmail(email);
+    if (error) {
+      console.error("error", error);
+      return {status: 500, data: {error: "Internal server error"}};
+    }
+    return {status: 201, data: {message: "Reset email sent"}};
   }
-
-  return {status: 200, data: {message: "Reset email sent"}};
+  return {
+    status: 200,
+    data: {
+      message:
+        "If email exists, you should get an email with instructions to reset your password \n Please check your email",
+    },
+  };
 }
 
 export default function ForgotPasswordRoute({
   actionData,
 }: Route.ComponentProps) {
-  console.log("actionData", actionData);
   return (
-    <div>
-      <h1>Forgot Password</h1>
-      <Form method="post">
-        <Label htmlFor="email">
-          Email
-          <Input type="email" name="email" required />
-        </Label>
-        <button type="submit">Send reset email</button>
-      </Form>
-      {actionData?.status === 200 && (
-        <>
-          <p>{actionData.data.message}</p>
-          <p>
-            You will receive an email with instructions to reset your password.
-          </p>
-        </>
-      )}
-    </div>
+    <PageWrapper>
+      <aside className="mb-5">
+        <H1>Forgot Password</H1>
+        <Lead>
+          Enter your email address and we will send you a link to reset your
+          password
+        </Lead>
+      </aside>
+      <div className="md:max-w-xl md:p-2">
+        <Form method="post">
+          <fieldset className="flex flex-col gap-2 rounded-md border-2 shadow md:p-2">
+            <legend>
+              <H2 className="font-bold text-xl">Reset Your Password</H2>
+            </legend>
+            <Label htmlFor="email">
+              Email
+              <Input type="email" name="email" required />
+            </Label>
+            <Button type="submit">Send reset email</Button>
+          </fieldset>
+        </Form>
+      </div>
+      <div className="min-h-[5rem]">
+        {actionData?.status === 200 ||
+          (actionData?.status === 201 && <p>{actionData.data.message}</p>)}
+      </div>
+    </PageWrapper>
   );
 }
--- app/routes/auth/reset-password.tsx
+++ app/routes/auth/reset-password.tsx
@@ -1,23 +1,36 @@
-import {Form} from "react-router";
-import {getUserByEmail} from "~/.server/db/dao/users";
+import {Form, Link, redirect} from "react-router";
+import {z} from "zod";
+import {getUserByEmail, updateUserPassword} from "~/.server/db/dao/users";
+import {hashPassword} from "~/.server/utils/password";
+import PageWrapper from "~/components/page-wrapper";
 import {Input} from "~/components/ui/input";
 import {Label} from "~/components/ui/typography";
 import {decrypt} from "~/lib/jwt/email-token";
+import {STATUS_CODE} from "~/lib/status-code";
 import type {Route} from "./+types/reset-password";
+
+export function meta() {
+  return [
+    {
+      title: "Reset Password",
+      description: "Reset Your Password Here",
+    },
+  ];
+}
 
 export async function loader({request}: Route.LoaderArgs) {
   let params = new URLSearchParams(request.url.split("?")[1]);
   let token = params.get("token");
   if (!token) {
-    throw new Error("Wrong credentials");
+    return redirect("/login");
   }
   let decryptedToken = await decrypt(token);
   if (decryptedToken === null) {
-    throw new Error("Invalid token");
+    return redirect("/login");
   }
   let user = await getUserByEmail(decryptedToken.email);
   if (user === null) {
-    throw new Error("User not found");
+    return redirect("/login");
   }
   return {
     status: 200,
@@ -27,14 +40,54 @@ export async function loader({request}: Route.LoaderArgs) {
   };
 }
 
+let ResetPasswordSchema = z.object({
+  password: z.string().min(6),
+  confirmPassword: z.string().min(6),
+  email: z.string().email(),
+});
+
 export async function action({request}: Route.ActionArgs) {
   let body = await request.formData();
-  return null;
+  let password = body.get("password");
+  let confirmPassword = body.get("confirmPassword");
+  let email = body.get("email");
+  let result = ResetPasswordSchema.safeParse({
+    password,
+    confirmPassword,
+    email,
+  });
+  if (!result.success) {
+    return {status: STATUS_CODE.BAD_REQUEST, data: {error: "Invalid data"}};
+  }
+  if (result.data.password !== result.data.confirmPassword) {
+    return {
+      status: STATUS_CODE.BAD_REQUEST,
+      data: {error: "Passwords do not match"},
+    };
+  }
+
+  // Update the user's password in the database
+  let hashedPassword = await hashPassword(result.data.password);
+  if (hashedPassword === null) {
+    return {
+      status: STATUS_CODE.INTERNAL_SERVER_ERROR,
+      data: {error: "Internal server error"},
+    };
+  }
+  let success = await updateUserPassword(hashedPassword, result.data.email);
+
+  return {
+    status: success ? STATUS_CODE.OK : STATUS_CODE.INTERNAL_SERVER_ERROR,
+    data: {success},
+  };
 }
 
-export default function ResetPasswordRoute() {
+export default function ResetPasswordRoute({
+  loaderData,
+  actionData,
+}: Route.ComponentProps) {
   return (
-    <div>
+    <PageWrapper>
       <h1>Reset Password</h1>
       <Form method="post">
         <Label htmlFor="password">
@@ -45,8 +98,19 @@ export default function ResetPasswordRoute() {
           Confirm Password
           <Input type="password" name="confirmPassword" required />
         </Label>
+        <input type="hidden" name="email" value={loaderData.data.email} />
         <button type="submit">Reset Password</button>
       </Form>
-    </div>
+      <div className="my-5 min-h-[5rem] ">
+        {actionData?.data.success && (
+          <div>
+            <p>Password reset successfully.</p>
+            <p>
+              You can now <Link to="/login">login</Link> with your new password.
+            </p>
+          </div>
+        )}
+      </div>
+    </PageWrapper>
   );
 }
