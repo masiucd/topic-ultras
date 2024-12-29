0d1e397454045af2546dc0a918f9b67d5ecc74a5
masiucd <ciszekmarcell@gmail.com>
2024-12-29 16:32:19 +0100

    feat: Implement authSessionV2 for enhanced session management and add token validation utility

--- app/.server/sessions.ts
+++ app/.server/sessions.ts
@@ -8,20 +8,54 @@ type SessionFlashData = {
   error: string;
 };
 
-export function authSession() {
-  let {getSession, commitSession, destroySession} = createCookieSessionStorage<
-    SessionData,
-    SessionFlashData
-  >({
-    cookie: {
-      name: "__session",
-      sameSite: "lax", // CSRF protection
-      httpOnly: true, // XSS protection
-      domain: "localhost", // make sure to change this to your domain
-      maxAge: 60 * 60, // 1 hour
-      secrets: ["s3cr3t"], // sign and verify the session
-      secure: process.env.env === "production", // only send the cookie over HTTPS
-    },
-  });
-  return {getSession, commitSession, destroySession};
-}
+let {getSession, commitSession, destroySession} = createCookieSessionStorage<
+  SessionData,
+  SessionFlashData
+>({
+  cookie: {
+    name: "__session",
+    sameSite: "lax", // CSRF protection
+    httpOnly: true, // XSS protection
+    domain: "localhost", // make sure to change this to your domain
+    maxAge: 60 * 60, // 1 hour
+    secrets: ["s3cr3t"], // sign and verify the session
+    secure: process.env.env === "production", // only send the cookie over HTTPS
+  },
+});
+
+export const authSessionV2 = async (
+  request: Request
+): Promise<{
+  userId: string;
+  commit: () => Promise<string>;
+  destroy: () => Promise<string>;
+  flash: (msg: string) => void;
+  set: (value: string) => void;
+  error?: string;
+} | null> => {
+  const session = await getSession(request.headers.get("Cookie"));
+  let userId = session.get("userId");
+  if (!userId) {
+    return null;
+  }
+
+  const commit = async () => {
+    return await commitSession(session);
+  };
+
+  const destroy = async () => {
+    return await destroySession(session);
+  };
+
+  const flash = (msg: string) => {
+    session.flash("error", msg);
+  };
+
+  const set = (value: string) => {
+    session.set("userId", value);
+  };
+
+  let error = session.get("error");
+
+  return {userId, commit, destroy, flash, set, error};
+};
