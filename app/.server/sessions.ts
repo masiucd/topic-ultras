0eb99b9aec39c04b436f32032b41f8d6ae826902
masiucd <ciszekmarcell@gmail.com>
2024-12-31 17:35:26 +0100

    refactor: Remove unused authSessionV2 function and export session management utilities

--- app/.server/sessions.ts
+++ app/.server/sessions.ts
@@ -23,39 +23,4 @@ let {getSession, commitSession, destroySession} = createCookieSessionStorage<
   },
 });
 
-export const authSessionV2 = async (
-  request: Request
-): Promise<{
-  userId: string;
-  commit: () => Promise<string>;
-  destroy: () => Promise<string>;
-  flash: (msg: string) => void;
-  set: (value: string) => void;
-  error?: string;
-} | null> => {
-  const session = await getSession(request.headers.get("Cookie"));
-  let userId = session.get("userId");
-  if (!userId) {
-    return null;
-  }
-
-  const commit = async () => {
-    return await commitSession(session);
-  };
-
-  const destroy = async () => {
-    return await destroySession(session);
-  };
-
-  const flash = (msg: string) => {
-    session.flash("error", msg);
-  };
-
-  const set = (value: string) => {
-    session.set("userId", value);
-  };
-
-  let error = session.get("error");
-
-  return {userId, commit, destroy, flash, set, error};
-};
+export {getSession, commitSession, destroySession};
