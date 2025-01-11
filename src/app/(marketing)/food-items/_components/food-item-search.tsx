c170b7dd544e9beb440e4081099614e92530cb03
masiucd <ciszekmarcell@gmail.com>
2025-01-11 17:47:34 +0100

    feat: Implement debounce functionality for food item search input to improve performance

--- src/app/(marketing)/food-items/_components/food-item-search.tsx
+++ src/app/(marketing)/food-items/_components/food-item-search.tsx
@@ -2,30 +2,47 @@
 import {Input} from "@/components/ui/input";
 import {usePathname, useSearchParams} from "next/navigation";
 import {useRouter} from "next/navigation";
+import {useCallback} from "react";
 
 export function FoodItemSearch() {
   let searchParams = useSearchParams();
-  // let params = useParams();
   let pathname = usePathname();
   let router = useRouter();
   let newSearchParams = new URLSearchParams(searchParams);
+
+  let handleSearchChange = useCallback(
+    debounce((searchTerm: string) => {
+      if (searchTerm === "") {
+        newSearchParams.delete("search");
+        let newUrl = `${pathname}?${newSearchParams.toString()}`;
+        router.push(newUrl);
+      } else {
+        newSearchParams.set("search", searchTerm);
+        let newUrl = `${pathname}?${newSearchParams.toString()}`;
+        router.push(newUrl);
+      }
+    }, 300),
+    []
+  );
+
   return (
     <Input
       className="w-full p-2"
       type="text"
       placeholder="Search for food items..."
-      onChange={(e) => {
-        let searchTerm = e.target.value;
-        if (searchTerm === "") {
-          newSearchParams.delete("search");
-          let newUrl = `${pathname}?${newSearchParams.toString()}`;
-          router.push(newUrl);
-        } else {
-          newSearchParams.set("search", searchTerm);
-          let newUrl = `${pathname}?${newSearchParams.toString()}`;
-          router.push(newUrl);
-        }
-      }}
+      onChange={(e) => handleSearchChange(e.target.value)}
     />
   );
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function debounce(fn: (...args: any[]) => void, delay: number) {
+  let timeoutId: NodeJS.Timeout;
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  return (...args: any[]) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
 }
