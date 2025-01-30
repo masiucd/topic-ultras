c0e35c427ecbb84f7de5e4d28c5cffcadc3bcda0
marcell ciszek <ciszekmarcell@gmail.com>
2025-01-30 14:59:36 +0100

    feat: Update configuration and dependencies for improved performance and testing

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    // dynamicIO: true,
     cacheLife: {
       footItems: {
         stale: 3600, // 1 hour
